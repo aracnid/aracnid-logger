7e35ed3e4cdfc78dd9b83140e4a20456f37bdbc9
Jason Romano <aracnid@gmail.com>
2022-11-02 08:04:32 -0400

    Revert version test

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,8 +1,8 @@
 """Test functions for Aracnid Logger import.
 """
-from aracnid_logger import __version__
+import aracnid_logger
 
 def test_version():
     """Tests that Aracnid Logger was imported successfully.
     """
-    assert __version__
+    assert aracnid_logger.__version__
